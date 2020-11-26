b5d99353f355551d1ebab4b6b0f51c5bdb81b247
AZFAR HOSSAIN <ahossain@us.ibm.com>
2020-11-26 18:08:34 -0500

    adding a new jenkins file to aac_pieline project 11/26 6:08 pm

---
+++ aac_pipeline.groovy
@@ -0,0 +1,15 @@
+//Declarative pipeline
+pipeline {
+  agent any
+  stages {
+    stage('clone sources') { 
+      steps {
+        git url: 'https://github.com/ahossain71/aac_pipeline.git'
+        }
+    }
+    stage('build souce')
+      steps{
+        mvn package
+      }
+  }
+}
