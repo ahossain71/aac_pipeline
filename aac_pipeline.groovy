0ef04bafc9ff05a8d745d1d2ff5f3b4643a8f1f2
AZFAR HOSSAIN <ahossain@us.ibm.com>
2020-11-26 22:08:02 -0500

    adding aac pieline code to repo for jenkins aac pipeline 11/26 10:07 pm

--- aac_pipeline.groovy
+++ aac_pipeline.groovy
@@ -1,16 +1,39 @@
 //Declarative pipeline
 pipeline {
   agent any
+  tools
+    {
+       maven "Maven"
+    }
   stages {
-    stage('checkout_application'){ 
-      steps {
-        git branch: 'main', url: 'https://github.com/ahossain71/trainingApp.git'
+      stage('checkout_application'){ 
+        steps {
+          git branch: 'main', url: 'https://github.com/ahossain71/trainingApp.git'
+          }
+      }
+
+      stage('Tools Init') {
+        steps {
+            script {
+                echo "PATH = ${PATH}"
+                echo "M2_HOME = ${M2_HOME}"
+            def tfHome = tool name: 'Ansible'
+            env.PATH = "${tfHome}:${env.PATH}"
+              sh 'ansible --version'
+            }
+        }
+      }
+
+    stage('Execute Maven') {
+            steps {
+              sh 'mvn package'             
+          }
+        }
+    stage('Ansible Deploy') {
+        steps {
+            sh "ansible-playbook deploy_trainingApp.yml -i inventories/dev/hosts:trainingweb -- user jenkins --key-file ~/.ssh/DevOpsKeyPair"
         }
     }
-    //stage('build souce'){
-    //  steps{
-        //mvn package
-    //  }
-    //}
-  }
-}
+
+  }// end stages
+}//end pipeline
