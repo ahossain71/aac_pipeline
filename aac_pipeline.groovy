34fb9144b25bc4601fa9e3e5850704d28e4dd45d
AZFAR HOSSAIN <ahossain@us.ibm.com>
2020-11-28 02:05:08 -0500

    removed the local hosst file and update the /etc/ansible/hosts file to include the traingweb group of hosts 11/28 2:05 am

--- aac_pipeline.groovy
+++ aac_pipeline.groovy
@@ -35,7 +35,7 @@ pipeline {
             //sh "ansible-playbook ./ansible/playbooks/deploy_trainingApp.yml --user ec2-user --key-file ~/.ssh/DevOpsKeyPair"
             //sh "ansible-playbook ./ansible/playbooks/deploy_trainingApp.yml --user ec2-user --key-file cff1d3fe-236f-43ca-8ff5-5f37ec63422d"   
          ///}
-        //steps {
+        //steps{
         //    withCredentials([sshUserPrivateKey(credentialsId: mySshKey, keyFileVariable: KEY')]) {
         //        sh "ssh -i ${KEY} 10.10.10.10 -C \'docker run -n my-nginx -p 8080:80 nginx\'"
         //  }
