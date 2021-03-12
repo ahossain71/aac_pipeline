4ec7d5c4c0b4cc85e5e4cb51cc412324a1ad3733
ahossain71 <azfar.hossain@yahoo.comgit config --global user.email azfar.hossain@yahoo.comgit config --global user.email azfar.hossain@yahoo.com>
2021-03-12 16:40:41 -0500

    Added stage for checking out applications to be built and deployed 0312440

--- aac_pipeline.groovy
+++ aac_pipeline.groovy
@@ -30,7 +30,7 @@ pipeline {
     stage('Ansible Deploy') {
         steps{
              withCredentials([sshUserPrivateKey(credentialsId: 'a59a13e3-8e2f-4920-83c9-a49b576e5d58', keyFileVariable: 'myTestKeyPair02')]) {
-                sh 'ansible-playbook ./ansible/playbooks/deploy_trainingApp.yml --user ubuntu --key-file ${myTestKeyPair02}'  
+                sh 'ansible-playbook $workspace/ansible/playbooks/deploy_trainingApp.yml --user ubuntu --key-file ${myTestKeyPair02}'  
             }//end withCredentials
       }//end steps
     }//end stage
