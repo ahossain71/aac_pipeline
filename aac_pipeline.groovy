ab9e9c2755616df1813ac0856e79285f233fa926
ahossain71 <azfar.hossain@yahoo.comgit config --global user.email azfar.hossain@yahoo.comgit config --global user.email azfar.hossain@yahoo.com>
2021-03-13 19:05:28 -0500

    trying to pull back ansible folder from github to the worksapce 0313705

--- aac_pipeline.groovy
+++ aac_pipeline.groovy
@@ -32,7 +32,7 @@ pipeline {
              withCredentials([sshUserPrivateKey(credentialsId: 'a59a13e3-8e2f-4920-83c9-a49b576e5d58', keyFileVariable: 'myTestKeyPair02')]) {
                 //sh 'wget --no-check-certificate --content-disposition https://github.com/ahossain71/aac_pipeline/tree/main/ansible'
                 //sh 'curl -LJO https://github.com/ahossain71/aac_pipeline/tree/main/ansible'
-                sh 'wget --no-check-certificate --content-disposition https://github.com/ahossain71/aac_pipeline/tree/main/ansible/playbooks/deploy_trainingApp.yml'
+                sh 'wget --no-check-certificate https://github.com/ahossain71/aac_pipeline/tree/main/ansible/playbooks/deploy_trainingApp.yml'
                 sh 'ansible-playbook deploy_trainingApp.yml --user ubuntu --key-file ${myTestKeyPair02}'  
             }//end withCredentials
       }//end steps
