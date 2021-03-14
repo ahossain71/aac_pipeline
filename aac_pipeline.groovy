5a38da7e0b2f87f171c788b2f06897a60d35b74d
ahossain71 <azfar.hossain@yahoo.comgit config --global user.email azfar.hossain@yahoo.comgit config --global user.email azfar.hossain@yahoo.com>
2021-03-13 22:11:57 -0500

    trying to pull back ansible folder from github to the worksapce 03131011

--- aac_pipeline.groovy
+++ aac_pipeline.groovy
@@ -32,15 +32,17 @@ pipeline {
              sh 'aws s3 cp ./target/training-tomcatweb-integration.war s3://application-pkgs/trainingApp/'
          }//end steps
     }//end stage
-    //stage('Ansible Deploy') {
-    //    steps{
-    //         withCredentials([sshUserPrivateKey(credentialsId: 'a59a13e3-8e2f-4920-83c9-a49b576e5d58', keyFileVariable: 'myTestKeyPair02')]) {
-                //sh 'wget --no-check-certificate --content-disposition https://github.com/ahossain71/aac_pipeline/tree/main/ansible'
-                //sh 'curl -LJO https://github.com/ahossain71/aac_pipeline/tree/main/ansible'
-                //sh 'wget  https://github.com/ahossain71/aac_pipeline/tree/main/ansible/playbooks/deploy_trainingApp.yml'
-    //            sh 'ansible-playbook deploy_trainingApp.yml --user ubuntu --key-file ${myTestKeyPair02}'  
-    //        }//end withCredentials
-    // }//end steps
-    //}//end stage
+    stage('checkout_training_playbooks'){ 
+        steps {
+          git branch: 'main', url: 'https://github.com/ahossain71/training_playbooks.git'
+          }
+      }
+    stage('Ansible Deploy') {
+        steps{
+          withCredentials([sshUserPrivateKey(credentialsId: 'a59a13e3-8e2f-4920-83c9-a49b576e5d58', keyFileVariable: 'myTestKeyPair02')]) {
+                sh 'ansible-playbook ./ansible/playbooks/deploy_trainingApp.yml --user ubuntu --key-file ${myTestKeyPair02}'  
+          }//end withCredentials
+     }//end steps
+    }//end stage
   }// end stages
 }//end pipeline
